b80aad359cba8eb4ff2b838eef059fb86d1c1ea8
lucafedri <luca.fedrizzi@belka.us>
2019-07-20 13:33:49 +0200

    Updates

---
+++ precache-manifest.4ad1d41f9a3a8b6ca7c033b2017098bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1436836e00d684d6f33becfffb64601a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "734336b3cfe874d852c5",
+    "url": "/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "29753e82a663ae3486d4",
+    "url": "/static/js/2.f82d50a6.chunk.js"
+  },
+  {
+    "revision": "734336b3cfe874d852c5",
+    "url": "/static/js/main.0d63aac1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
