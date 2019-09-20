3b712278ceb1425666c8a4cab13bbf72592d5bfa
lucafedri <luca.fedrizzi@belka.us>
2019-09-20 12:37:43 +0200

    Add reac router to docs

--- docs/src/docRoadmap/index.tsx
+++ docs/src/docRoadmap/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { SectionTitle } from "../App";
+import { SectionTitle } from "../routes/components/typography";
 
 export const DocRoadmap = () => {
   return (
---
+++ docs/src/routes/components/typography.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+  text-align: center;
+`;
+export const Subtitle = styled.h2`
+  text-align: center;
+`;
+export const SectionTitle = styled.h3`
+  border: 1px solid #444;
+  padding: 1rem;
+  margin-top: 2rem;
+`;
